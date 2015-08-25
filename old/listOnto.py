be521962e7ec26a45feadd88955b9e5f296cc80f
mkamdar <mkamdar@DN52eca6.SUNet>
2015-08-25 09:16:07 -0700

    Fresh update as last one was desperately created post laptop crash

--- old/listOnto.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-listOntoFile = open("listOntoAll.tsv", "w+")
-
-for root, dirs, files in os.walk(u"ontologies/"):
-    for file in files:
-        listOntoFile.write(file.split("--")[0] + "\n")
-     
